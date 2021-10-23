c6dfaf053f65a37e2dd3b5492bc2bf87ec90468b
Anne <anne.mboke@epitech.eu>
2021-10-24 00:53:26 +0200

    [MERGE]

--- src/TheGrammer.hs
+++ src/TheGrammer.hs
@@ -6,10 +6,9 @@ module TheGrammer
  )
 where
 import Bootstrap
-import OurMagicParser
 
 oper:: Char -> Float -> Float -> Float
-oper ' ' x y = 2 / 0
+oper ' ' x y = x + y 
 oper '-' x y = x - y
 oper '+' x y = x + y
 oper '*' x y = x * y
