e54d747ebb0b09de211c1507e88ccb6d9fc97f36
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-10-24 01:29:28 +0200

    just to be sure

---
+++ src/OurMagicParser.hs
@@ -0,0 +1,32 @@
+module OurMagicParser
+    (
+       
+        braveExit
+        , printResult
+        , stripChars
+    ) where
+
+import Text.Printf
+import System.Exit
+
+stripChars :: String -> String -> String
+stripChars = filter . flip notElem
+
+
+printResult :: [(Float, String)] -> IO()
+printResult [] = braveExit "I could not calculate... I am sorry" 84
+printResult  res | snd (head res) /= "" =  braveExit "I could not parse everything.." 84
+                 | otherwise = 
+                   thePrint $ fst (head res) 
+
+
+braveExit :: String -> Int -> IO ()
+braveExit str 0 = putStrLn str >> exitWith (ExitSuccess)
+braveExit str n = putStrLn str >> exitWith (ExitFailure n)
+
+
+thePrint :: (Show a, PrintfArg a, RealFloat a) => a -> IO ()
+thePrint x = 
+    if (not (isInfinite x)) then
+        printf "%.2f\n" x
+    else braveExit "I think you have tried to divide by zero .." 84
---
+++ src/TheGrammer.hs
@@ -0,0 +1,51 @@
+module TheGrammer
+ (
+     module TheGrammer
+     ,module Bootstrap
+
+ )
+where
+import Bootstrap
+
+oper:: Char -> Float -> Float -> Float
+oper ' ' x y = x + y 
+oper '-' x y = x - y
+oper '+' x y = x + y
+oper '*' x y = x * y
+oper '/' x y = x / y
+
+
+expression :: NParser Float
+expression = 
+       do
+       x <- term
+       op <- (char '+' <|> char '-')
+       y <- expression    
+       return (oper op x y)
+       <|> term
+
+
+term::NParser Float
+term = do
+       x <- factor
+       op <- char '*' <|> char '/'
+       y <- term
+       return (oper op x y)
+       <|> factor 
+
+factor::NParser Float
+factor = do
+    x <- pow
+    _ <- char '^'
+    y <- factor
+    return (x ** y)
+    <|> pow
+
+pow::NParser Float
+pow = do
+      --sign <- getFrontSign
+      _ <- char '('
+      x <- expression
+      _ <- char ')'
+      return x
+      <|> flt
